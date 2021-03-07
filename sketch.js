732022ece699277701e75337a973d0c784b5c3da
Yuri Jordan <yurirdn@ufrn.edu.br>
2021-03-07 16:00:27 -0300

    integra os comandos do teachable com o p5 (os comandos são dedo indicador - Esq, dois dedos - Dir)

--- sketch.js
+++ sketch.js
@@ -1,62 +1,62 @@
-//Collision detection and resolution
-//move the mouse, the sprite responds to overlapings, collision,
-//and displaces another sprite
 
-var box, asterisk, cloud, circle;
+  // Classifier Variable
+  let classifier;
+  // Model URL
+  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/EQskIKETN/';
+  
+  // Video
+  let video;
+  let flippedVideo;
+  // To store the classification
+  let label = "";
 
-function setup() {
-  createCanvas(800, 400);
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  }
 
-  //create 4 sprites
-  circle = createSprite(400, 200);
-  //compact way to add an image
-  circle.addImage(loadImage('assets/plain_circle.png'));
+  function setup() {
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
 
-  box = createSprite(200, 200);
-  box.addAnimation('normal', 'assets/box0001.png', 'assets/box0003.png');
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+  }
 
-  cloud = createSprite(600, 200);
-  cloud.addAnimation('normal', 'assets/cloud_breathing0001.png', 'assets/cloud_breathing0009.png');
+  function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
 
-  asterisk = createSprite(200, 200);
-  asterisk.addAnimation('normal', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+  }
 
-  asterisk.addAnimation('round', 'assets/asterisk_circle0006.png', 'assets/asterisk_circle0008.png');
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
 
+  }
 
-}
-
-function draw() {
-  background(255, 255, 255);
-
-  asterisk.position.x = mouseX;
-  asterisk.position.y = mouseY;
-
-  //check and resolve the inteactions between sprites
-
-  //sprite.overlap() returns true if overlapping occours
-  //note: by default the check is performed on the images bounding box
-  //press mouse button to visualize them
-  if(asterisk.overlap(circle))
-    asterisk.changeAnimation('round');
-  else
-    asterisk.changeAnimation('normal');
-
-  //collide also returns a true/false but it can simply be used to
-  //resolve collisions.
-  //If overlapping with box asterisk will be placed
-  //in the closest non overlapping position
-  asterisk.collide(box);
-
-  //displace is the opposite of collide, the sprite in the parameter will
-  //be pushed away but the sprite calling the function
-  asterisk.displace(cloud);
-
-  //if debug is set to true bounding boxes, centers and depths are visualized
-  asterisk.debug = mouseIsPressed;
-  circle.debug = mouseIsPressed;
-  box.debug = mouseIsPressed;
-  cloud.debug = mouseIsPressed;
-
-  drawSprites();
-}
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classifiy again!
+    classifyVideo();
+  }
